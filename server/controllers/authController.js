bec8dcef9e179864cee7d20a2d16620490dc5f4a
Kumar-Shivam01 <pkshivam9@gmail.com>
2026-08-13 01:51:00 +0530

    feat(auth): implement email verification process with OTP functionality

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,6 @@
-const User = require('../models/userModel.js');
-const jwt = require('jsonwebtoken');
-const sendEmail = require('./../config/email.js')
+const User = require("../models/userModel.js");
+const jwt = require("jsonwebtoken");
+const sendEmail = require("./../config/email.js");
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.SECRET_STR, {
     expiresIn: process.env.JWT_EXPIRE,
@@ -15,7 +15,7 @@ exports.register = async (req, res) => {
       status: "fail",
       message: "Missing details",
     });
-  }   
+  }
   try {
     const existingUser = await User.findOne({ email });
     if (existingUser) {
@@ -33,12 +33,12 @@ exports.register = async (req, res) => {
       sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
       maxAge: 7 * 24 * 60 * 60 * 1000, //7 days in milliseconds
     });
-    
+
     await sendEmail({
-        email: email,
-        subject: 'Welcome to Auth_MERN',
-        message: `Welcome to Auth_MERN website. Your account has been created with the email id: ${email}`
-    }); 
+      email: email,
+      subject: "Welcome to Auth_MERN",
+      message: `Welcome to Auth_MERN website. Your account has been created with the email id: ${email}`,
+    });
 
     res.status(201).json({
       status: "success",
@@ -97,55 +97,100 @@ exports.login = async (req, res) => {
   }
 };
 
-exports.logout = async (req,res)=>{
-  try{
-    res.clearCookie('rememberme',{
+exports.logout = async (req, res) => {
+  try {
+    res.clearCookie("rememberme", {
       httpOnly: true,
       secure: process.env.NODE_ENV == "production",
       sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-    })
+    });
     return res.status(200).json({
       status: "success",
-      message: "logged out"
+      message: "logged out",
     });
-  }catch(error){
+  } catch (error) {
     res.status(500).json({
       status: "fail",
       message: error.message,
     });
   }
-}
+};
 //send verification OPT to the user's email
-exports.sendVerifyOtp = async (req,res)=>{
-  try{  
-    const {id} = req.body;
-    const user = await User.findById(id)
-    if(user.isAccountVerified){ //check if the user is already verified
+exports.sendVerifyOtp = async (req, res) => {
+  try {
+    const { id } = req.body;
+    const user = await User.findById(id);
+    if (user.isAccountVerified) {
+      //check if the user is already verified
       return res.json({
-        status: 'false',
-        message: 'Account is already verified!'
-      })
+        status: "false",
+        message: "Account is already verified!",
+      });
     }
-    const otp = String(Math.floor(100000 + Math.random()*900000)) //generating a random otp
+    const otp = String(Math.floor(100000 + Math.random() * 900000)); //generating a random otp
     user.verifyOtp = otp;
-    user.verifyOtpExpireAt = Date.now() + 1*60*60*1000; //otp expires in 1 hour
+    user.verifyOtpExpireAt = Date.now() + 1 * 60 * 60 * 1000; //otp expires in 1 hour
 
     await user.save(); //saving the updated document
 
-    await sendEmail({ //setting email options
-        email: user.email,
-        subject: 'Account verification OTP',
-        message: `your otp is ${otp}. Verify your account using this otp.`
+    await sendEmail({
+      //setting email options 
+      email: user.email,
+      subject: "Account verification OTP",
+      message: `your otp is ${otp}. Verify your account using this otp.`,
     });
     res.status(200).json({
-      status: 'success',
-      message: 'verification otp sent on your mail!'
-    })
-  }catch(error){
+      status: "success",
+      message: "verification otp sent on your mail!",
+    });
+  } catch (error) {
     res.status(500).json({
-      status: 'fail',
-      message: error.message
-    })
+      status: "fail",
+      message: error.message,
+    });
   }
-}
+};
+exports.verifyEmail = async (req, res) => {
+  const { id, otp } = req.body;
+  if (!id || !otp) {
+    return res.status(400).json({
+      status: "fail",
+      message: "Missing user details",
+    });
+  }
+  try {
+    const user = await User.findById(id);
+    if(!user){
+      return res.status(404).json({
+        status: 'fail',
+        message: 'User not found'
+      })
+    }
+    if(user.verifyOtp === '' || user.verifyOtp !== otp){
+      return res.status(404).json({
+        status: 'fail',
+        message: 'Invalid OTP'
+      })
+    }
+    if(user.verifyOtpExpireAt < Date.now()){
+      return res.status(404).json({
+        status: 'fail',
+        message: 'OTP has expired'
+      })
+    }
+    user.isAccountVerified = true;   
+    user.verifyOtp = ''
+    user.verifyOtpExpireAt = 0
 
+    await user.save()
+    return res.status(200).json({
+        status: 'success',
+        message: 'Email verified successfully!'
+    })
+  } catch (error) {
+    res.status(500).json({
+      status: "fail",
+      message: error.message,
+    });
+  }
+};
