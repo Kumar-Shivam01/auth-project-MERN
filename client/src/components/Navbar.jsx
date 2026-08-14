fb17b40523587b5835eab04146b1d7f061b35c4c
Kumar-Shivam01 <pkshivam9@gmail.com>
2026-08-15 01:09:12 +0530

    feat: enhance Navbar and Login components with routing and user interface improvements

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,9 @@
 import logo from '../assets/web-app-manifest.png'
+import { useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
+  const navigate = useNavigate();
+
   return (
     <nav className='absolute inset-x-0 top-0 z-10 flex w-full items-center justify-between p-4 sm:p-6 sm:px-24 mb-10'>
         <div className='flex items-center'>
@@ -8,7 +11,7 @@ const Navbar = () => {
             <span className='ml-3 min-w-40 text-4xl font-bold tracking-wide text-black sm:min-w-56 sm:text-3xl'>Auth</span>
         </div>
         
-        <button className='flex items-center gap-2 rounded-full border border-gray-500 px-4 py-2 text-black hover:bg-white/10'>
+        <button onClick={()=>navigate('/login')} className='flex items-center gap-2 rounded-full border border-gray-500 px-4 py-2 text-black hover:bg-white/10'>
           Login <span aria-hidden="true">→</span>
         </button>
     </nav>
