2a9f31d9f945451c480a8b22147a60d82d097505
Kumar-Shivam01 <pkshivam9@gmail.com>
2026-08-18 23:52:33 +0530

    Implements multi-step password reset UI flow
    The page now includes:
    - A form for submitting the user's email address.
    - A dedicated OTP verification form with intelligent input fields that auto-focus, allow backspace navigation, and support pasting a 6-digit code.
    - A final form for setting a new password.

--- client/src/pages/ResetPassword.jsx
+++ client/src/pages/ResetPassword.jsx
@@ -1,8 +1,32 @@
+import { useState } from "react";
 import logo from "../assets/web-app-manifest.png";
 import { useNavigate } from "react-router-dom";
-
+import React from "react";
 const ResetPassword = () => {
   const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [newPassword,setNewPassword] = useState('')
+  const inputRefs = React.useRef([]);
+
+  const handleInput = (e, index) => {
+    if (e.target.value.length > 0 && index < inputRefs.current.length - 1) {
+      inputRefs.current[index + 1].focus();
+    }
+  };
+  const handleKeyDown = (e, index) => {
+    if (e.key === "Backspace" && e.target.value === "" && index > 0) {
+      inputRefs.current[index - 1].focus();
+    }
+  };
+  const handlePaste = (e) => {
+    const paste = e.clipboardData.getData("text");
+    const pasteArray = paste.split("");
+    pasteArray.forEach((char, index) => {
+      if (inputRefs.current[index]) {
+        inputRefs.current[index].value = char;
+      }
+    });
+  };
   return (
     <div className="flex items-center justify-center min-h-screen px-6 sm:px-0 bg-linear-to-br  from-white to-blue-400">
       <div className="absolute left-5 top-5 flex items-center sm:left-20">
@@ -19,6 +43,7 @@ const ResetPassword = () => {
           Auth
         </span>
       </div>
+      {/* Enter email id */}
       <form className="bg-slate-900 p-8 rounded-lg w-96 text-sm">
         <h1 className="text-white text-2xl font-semibold text-center mb-4">
           Reset Password
@@ -28,8 +53,69 @@ const ResetPassword = () => {
         </p>
         <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]">
           <span>📩</span>
-          <input type="email" placeholder="Email id" className="bg-transparent outline-none text-white"/>
+          <input
+            type="email"
+            placeholder="Email id"
+            className="bg-transparent outline-none text-white"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
         </div>
+        <button className="w-full py-2.5 bg-linear-to-r from bg-indigo-500 to-indigo-900 text-white rounded-full mt-3">
+          Submit
+        </button>
+      </form>
+      {/* OTP input form */}
+      <form className="bg-slate-900 p-8 rounded-lg w-96 text-sm" action="">
+        <h1 className="text-white text-2xl font-semibold text-center mb-4">
+          Reset Password OTP
+        </h1>
+        <p className="text-center mb-6 text-indigo-300">
+          Enter the 6-digit code sent to your Email Id.
+        </p>
+        <div className="flex justify-between mb-8" onPaste={handlePaste}>
+          {Array(6)
+            .fill(0)
+            .map((_, index) => (
+              <input
+                className="w-12 h-12 bg-[#333A5C] text-white text-center text-xl rounded-md"
+                type="text"
+                maxLength="1"
+                key={index}
+                required
+                ref={(e) => (inputRefs.current[index] = e)}
+                onInput={(e) => handleInput(e, index)}
+                onKeyDown={(e) => handleKeyDown(e, index)}
+              />
+            ))}
+        </div>
+        <button className="w-full py-2.5 bg-linear-to-r from-indigo-500 to-indigo-900 text-white rounded-full">
+          Reset Password
+        </button>
+      </form>
+      {/* Enter new password */}
+      <form className="bg-slate-900 p-8 rounded-lg w-96 text-sm">
+        <h1 className="text-white text-2xl font-semibold text-center mb-4">
+          New Password
+        </h1>
+        <p className="text-center mb-6 text-indigo-300">
+          Enter the new password below.
+        </p>
+        <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]">
+          <span>🔐</span>
+          <input
+            type="password"
+            placeholder="Password"
+            className="bg-transparent outline-none text-white"
+            value={newPassword}
+            onChange={(e) => setNewPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button className="w-full py-2.5 bg-linear-to-r from bg-indigo-500 to-indigo-900 text-white rounded-full mt-3">
+          Submit
+        </button>
       </form>
     </div>
   );
